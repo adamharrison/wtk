a9c72e392f2f4593073108cd667bede650582117
Adam Harrison <adamdharrison@gmail.com>
2025-12-22 17:36:21 -0500

    Final commit for now.

--- utilities/wtkjq/build.sh
+++ utilities/wtkjq/build.sh
@@ -1,1 +1,2 @@
-gcc -s -O2 `pkg-config --cflags lua5.4` main.c ../../src/wtk.c ../../src/json.c `pkg-config --libs lua5.4` -lm  -static -o wtkjq
+lua5.4 ../../scripts/pack.lua init.lua > packed.c
+gcc -DPACKED_LUA -s -O2 `pkg-config --cflags lua5.4` *.c ../../src/wtk.c ../../src/json.c `pkg-config --libs lua5.4` -lm  -static -o wtkjq
