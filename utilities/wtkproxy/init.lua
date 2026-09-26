8c19446d61b2fb4f33fa158c8358d6cc5c1febe4
Adam Harrison <adamdharrison@gmail.com>
2026-09-25 22:07:55 -0400

    Almost done with ACME.

--- utilities/wtkproxy/init.lua
+++ utilities/wtkproxy/init.lua
@@ -1,11 +1,82 @@
 local wtk = require "wtk.c"
 local system = require "wtk.c.system"
 local json = require "wtk.json.c"
+local proc = require "wtk.proc.c"
 local Server = require "wtk.server"
 local Client = require "wtk.client.c"
+local base64 = (require "wtk.server.c").base64
 
 local function arrayify(t) if type(t) == 'table' then return t else return { t } end end
 local function merge(...) local r = {} for _, t in ipairs({ ... }) do for k, v in pairs(t) do r[k] = v end end return r end
+local function filter(func, t) local r = {} for i, v in ipairs(t) do if func(v,i) then table.insert(r, v)  end end return r end
+local function map(func, t) local r = {} for i, v in ipairs(t) do table.insert(r, func(v,i)) end return r end
+local function base64url(data) return base64.encode(data):gsub("%+", "-"):gsub("/", "_"):gsub("=", "") end
+
+assert(ACME, "requires ACME to be defined by main.c")
+ACME.__index = ACME
+-- https://datatracker.ietf.org/doc/rfc8555/
+function ACME.new(options) return setmetatable(merge({ test = false, log = Server.Log.new(), configdir = nil, private_key = nil, poll_interval = 5, client = Client.new({ cookies = false }), nonce = nil, token = function(domain, token, body) end }, options), ACME) end
+function ACME:resolve(path) if path:find("^http") then return path end return string.format("https://%s.api.letsencrypt.org/acme%s", self.test and "acme-staging-v02" or "acme-v02", path) end
+function ACME:jwk() local components = assert(ACME.components(self.private_key)) return { kty = "RSA", n = base64url(components.n), e = base64url(components.e) } end
+function ACME:request(url, payload)
+  local request_body = { 
+    protected = base64url(json.encode(merge({ 
+      alg = "RS256", 
+      nonce = self.nonce or self:get_nonce(), 
+      url = self:resolve(url) 
+    }, self.account and { 
+      kid = self.account 
+    } or {
+      jwk = self:jwk()
+    }))),
+    payload = payload and base64url(json.encode(payload)) or ""
+  }
+  request_body.signature = base64url(assert(ACME.sign(self.private_key, request_body.protected .. "." .. request_body.payload)))
+  local url = self:resolve(url)
+  self.log:verbose("POST %s", url)
+  local body, res = self.client:post(url, json.encode(request_body), {}, { ['Content-Type'] = "application/jose+json" })
+  self.nonce = assert(res.headers['replay-nonce'], "can't find Replay-Nonce")
+  self.log:verbose("< %s", body)
+  return json.decode(body), res
+end
+function ACME:get_nonce() self.log:verbose("HEAD %s", self:resolve("/new-nonce")) return assert(self.client:head(self:resolve("/new-nonce")).headers['replay-nonce'], "unable to get nonce") end
+function ACME:get_account(email) -- by the ACME standard, this will not create a new account, if using the same public key
+  local body, res = self:request("/new-acct", { termsOfServiceAgreed = true, contact = { "mailto:" .. email } })
+  return assert(res.headers.location, "can't find account")
+end
+function ACME:create_order(domains, options) return self:request("/new-order", { identifiers = map(function(e) return { type = "dns", value = e } end, type(domains) == 'table' and domains or domains), notBefore = options.notBefore, notAfter = options.notAfter }) end
+function ACME:get_certificate(email, key, domains, options) 
+  options = options or {}
+  self.account = self:get_account(email)
+  local result, req = self:create_order(domains, options)
+  local order = req.headers.location
+  if not result.certificate and result.authorizations then
+    for i, v in ipairs(result.authorizations) do
+      while true do
+        local authorization = self:request(v)
+        local domain = authorization.identifier.value
+        if authorization.status == "pending" then
+          local challenge = assert(filter(function(e) return e.type == "http-01" end, authorization.challenges)[1], "cannot find http-01 challenge")
+          local func = options.token or self.token
+          local jwk = self:jwk()
+          func(domain, challenge.token, challenge.token .. "." .. ACME.sha256(string.format('{"e":"%s","kty":"RSA","n":"%s"}', jwk.e, jwk.n)))
+          self:request(challenge.url, json.empty_object)
+        elseif authorization.challenge == "valid" then
+          break
+        end
+        coroutine.yield(options.poll_interval or self.poll_interval)
+      end
+    end
+    if not result.certificate then
+      self:request(result.finalize, ACME.csr(key, arraify(domains)))
+    end
+    while not result.certificate do
+      result = self:request(order)
+      coroutine.yield(options.poll_interval or self.poll_interval)
+    end
+    return self:request(result.certificate)
+  end
+end
 
 local loop = wtk.Loop.new()
 local args = wtk.pargs({ ... }, {
@@ -17,6 +88,8 @@ local args = wtk.pargs({ ... }, {
   console = "flag",
   host = "string",
   port = "string",
+  acme = "string",
+  live = "flag",
   handler = "string"
 })
 if args.version then
@@ -35,7 +108,8 @@ The following options are available:
   --version             show the version
   --debug               enables debug mode
   --config              read and follow specified configuration a-la-nginx
-  --cert                uses the ACME protocol to generate/renew a certificate
+  --live                monitor the config file for changes, and autoamtically reload on modification
+  --acme                uses the ACME protocol to generate/renew certificates as needed for those servers don't have one; takes an email
   --help                show the help
 
   In order to forward requests, you have a couple options.
@@ -56,8 +130,8 @@ The following options are available:
         "hosts": [{
           "hostname": ["www.test.com", "test.com"],
           "ssl": {
-            "key": "/var/www/server/key.key",
-            "cert": "/var/www/server/cert.crt"
+            "key_path": "/var/www/server/key.key",
+            "cert_path": "/var/www/server/cert.crt"
           },
           "execute": {
             "bin": ["/var/www/server"],
@@ -65,10 +139,7 @@ The following options are available:
           }
         }, {
           "hostname": ["www.test2.com", "test2.com"],
-          "ssl": {
-            "key": "/var/www/server/key.key",
-            "cert": "/var/www/server/cert.crt"
-          },
+          "ssl": true,
           "forward": "http://127.0.0.1:4765"
         }, {
           "hostname": [".*%.test3%.com"],
@@ -140,7 +211,7 @@ function Server.Client:handshake(client)
     while true do
       local status, err = client.socket:handshake(function(hostname)
         local host = assert(self:get_host(hostname), "can't find host " .. hostname)
-        return assert(host.ssl, "can't find ssl key"), assert(host.ssl, "can't find ssl cert")
+        return assert(host.ssl.key, "can't find ssl key"), assert(host.ssl.cert, "can't find ssl cert")
       end)
       if status then break end
       client:yield(assert(err == "write" or err == "read" and err, err))
@@ -153,8 +224,16 @@ local proxy = {
   agent = Client.new({ cookies = false }),
   log = Server.Log.new(args.verbose),
   servers = {},
+  challenges = {},
   handler = function(self, request)
     local host = assert(self:get_host(request.headers.host), { code = 404, message = "can't find host " .. (request.headers.host or "unknown") })
+    if proxy.challenges[host] then 
+      local token = request.path:match("/.well-known/acme-challenge/([^/]+)")
+      if token and proxy.challenges[token] then 
+        proxy.log:info("Responding to challenge for %s at %s.", host, request.path)
+        return request:respond(200, { ['Content-Type'] = "application/octet-stream" }, proxy.challenges[token])
+      end
+    end
     local location, remainder = self:get_location(host, request)
     local target = location or host
     if target.forward then
@@ -170,38 +249,110 @@ local proxy = {
   end
 }
 
+local function decode_hosts(server)
+  if server.ssl then
+    for i, host in ipairs(server.hosts) do
+      if host.ssl then
+        if host.ssl == true then host.ssl = {} end
+        if not host.ssl.key and host.ssl.key_path then host.ssl.key = assert(wtk.io.file(host.ssl.key_path, "rb")):read("*all") assert(ACME.component(host.ssl.key), "key specified at " .. host.ssl.key_path .. " is invalid") end
+        if not host.ssl.cert and host.ssl.cert_path then host.ssl.cert = assert(wtk.io.file(host.ssl.cert_path, "rb")):read("*all") assert(ACME.cert(host.ssl.cert), "cert specified at " .. host.ssl.cert_path .. " is invalid") end
+        if host.hostname then host.hostname = arrayify(host.hostname) end
+      end
+    end
+  end
+  return server
+end
+
 local function load_config(path)
+  local add_acme = args.acme
   proxy.log:info("Loading configuration from %s...", path)
   for i, server in ipairs(proxy.servers) do server:stop(loop) end
   proxy.servers = {}
-  local config = assert(json.decode(assert(io.open(path, "rb")):read("*all")))
+  local config = assert(json.decode(assert(wtk.io.file(path, "rb")):read("*all")))
   for _, server in ipairs(config.servers) do
     for _, http in ipairs(arrayify(server.http)) do
       local bind, port = http:match("^([^:]+):([^:]+)$")
+      if port == 80 then add_acme = false end
       assert(bind, "can't decode bind " .. http)
       if port then port = tonumber(port) end
-      table.insert(proxy.servers, Server.new(merge(args, { port = port, host = bind, hosts = server.hosts }, proxy)):add(loop))
+      table.insert(proxy.servers, decode_hosts(Server.new(merge(args, { port = port, host = bind, hosts = server.hosts, handler = handler }, proxy))):add(loop))
     end
     for _, https in ipairs(arrayify(server.https)) do
       local bind, port = https:match("^([^:]+):([^:]+)$")
       assert(bind, "can't decode bind " .. https)
       if port then port = tonumber(port) end
-      table.insert(proxy.servers, Server.new(merge(args, { port = port, host = bind, ssl = true, hosts = server.hosts }, proxy)):add(loop))
+      table.insert(proxy.servers, decode_hosts(Server.new(merge(args, { port = port, host = bind, ssl = true, hosts = server.hosts, handler = handler }, proxy))):add(loop))
     end
   end
+  if add_acme then table.insert(proxy.servers, Server.new(merge(args, { name = "ACME Server", port = 80, host = "0.0.0.0", handler = handler })):add(loop)) end
 end
 
 if args.config then 
   load_config(args.config)
   loop:signal(SIGHUP, function() load_config(args.config) end)
+  if args.live then
+    loop:job(function() 
+      local mtime = assert(system.stat(args.config), "can't find config file").mtime
+      while true do
+        local nmtime = assert(system.stat(args.config), "can't find config file").mtime
+        if mtime < nmtime then
+          load_config(args.config)
+          mtime = nmtime
+        end
+        coroutine.yield(1)
+      end
+    end)
+  end
 else
   Server.new(merge(proxy, args, { handler = handler })):add(loop)
 end
 
-local ACME = { test = false }
-function ACME:ask_certify_domain(domain) end
-function ACME:complete_certify_domain(domain) end
-function ACME:issue_certificate(domain, public_key) end
+
+if args.acme then
+  proxy.acme = ACME.new({ token = function(domain, token, body)
+    if not proxy.challenges[domain] then proxy.challenges[domain] = {} end
+    proxy.challenges[domain][token] = body
+  end, lenience = 30*24*60*60, directory = "./.acme", test = true, log = proxy.log })
+  assert(args.acme:match("%w@%w+%.%w+"), "--acme should take an email")
+  loop:job(function() 
+    while true do
+      proxy.log:info("Initializing ACME loop...")
+      if not system.stat(proxy.acme.directory) then assert(system.mkdir(proxy.acme.directory)) end
+      local key_path = proxy.acme.directory .. "/lets-encrypt.key"
+      local certificate_directory_path = proxy.acme.directory .. "/certificates"
+      if not system.stat(key_path) then 
+        proxy.log:info("Generating ACME private key, storing at %s.", key_path)
+        proxy.acme.private_key = proc.run(function() local private_key = assert(ACME.keypair()) io.stdout:write(private_key):close() end)
+        assert(wtk.io.file(key_path, "wb")):write(proxy.acme.private_key):close()
+      else
+        proxy.acme.private_key = assert(wtk.io.file(key_path, "rb")):read("*all")
+      end
+      assert(ACME.components(proxy.acme.private_key))
+      if not system.stat(certificate_directory_path) then assert(system.mkdir(certificate_directory_path)) end
+      for _, server in ipairs(proxy.servers) do
+        if server.ssl then
+          for _, host in ipairs(server.hosts) do
+            if host.ssl then
+              local hostnames = filter(function(h) return not h:find("%*") end, host.hostname)
+              if not host.ssl.key then host.ssl.key = proxy.acme.private_key end
+              if #hostnames > 0 and (not host.ssl.cert or (os.time() - assert(ACME.cert(host.ssl.cert)).valid) < proxy.acme.lenience) then
+                proxy.log:info("%s SSL certificate for %s...", host.ssl.cert and "Generating" or "Renewing", table.concat(host.hostname, ", "))
+                host.ssl.cert = proxy.acme:get_certificate(args.acme, key, host.hostname)
+                local target = host.ssl.cert_path or certificate_directory_path .. "/" .. host.hostname[1] .. ".crt"
+                proxy.log:info("Successfully renewed SSL certificate for %s; writing to %s.", table.concat(arrayify(host.hostname), ", "), target)
+                assert(wtk.io.file(target, "wb")):write(host.ssl.cert):close()
+              end
+            end
+          end
+        end
+      end
+      coroutine.yield(60*60)
+    end
+  end):fail(function(err) 
+    proxy.log:error("Error during ACME loop: %s", err)
+    proxy.log:verbose(err.stack)
+  end)
+end
 
 
 loop:run()
--- utilities/wtkproxy/main.c
+++ utilities/wtkproxy/main.c
@@ -4,29 +4,232 @@
 #include <server.c>
 #include <client.c>
 #include <json.c>
+#include <proc.c>
 #include <stdio.h>
+#include <mbedtls/x509_csr.h>
+#include <mbedtls/sha256.h>
 
 #ifndef WTKPROXY_VERSION
   #define WTKPROXY_VERSION "unknown"
 #endif
 
-static int f_generate_keypair(lua_State* L) {
+// https://github.com/Mbed-TLS/mbedtls/blob/mbedtls-3.6/programs/pkey/gen_key.c
+// https://github.com/Mbed-TLS/mbedtls/blob/mbedtls-3.6/programs/pkey/rsa_genkey.c
+static int lua_setmpi(lua_State* L, int index, mbedtls_mpi* mpi, const char* key) {
+  unsigned char component_buf[MBEDTLS_MPI_MAX_SIZE] = {0};
+  size_t length = mbedtls_mpi_size(mpi);
+  mbedtls_mpi_write_binary(mpi, component_buf, length);
+  lua_pushlstring(L, component_buf, length);
+  lua_setfield(L, -2, key);
   return 0;
 }
-
-static int f_generate_cert(lua_State* L) {
-  return 0;
+static int f_components(lua_State* L) {
+  mbedtls_mpi N, P, Q, D, E, DP, DQ, QP;
+  mbedtls_mpi_init(&N); mbedtls_mpi_init(&P); mbedtls_mpi_init(&Q);
+  mbedtls_mpi_init(&D); mbedtls_mpi_init(&E); mbedtls_mpi_init(&DP);
+  mbedtls_mpi_init(&DQ); mbedtls_mpi_init(&QP);
+  mbedtls_entropy_context entropy;
+  mbedtls_entropy_init(&entropy);
+  mbedtls_ctr_drbg_context ctr_drbg;
+  mbedtls_ctr_drbg_init(&ctr_drbg);
+  mbedtls_pk_context key;
+  mbedtls_pk_init(&key);
+  size_t private_key_length;
+  const char* private_key = luaL_checklstring(L, 1, &private_key_length);
+  lua_newtable(L);
+  int ret = 0;
+  const unsigned char* pers = "components";
+  int failure = 
+    (ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, pers, strlen(pers))) != 0 ||
+    (ret = mbedtls_pk_parse_key(&key, private_key, private_key_length + 1, NULL, 0, mbedtls_ctr_drbg_random, &ctr_drbg)) != 0 ||
+    (ret = mbedtls_rsa_export(mbedtls_pk_rsa(key), &N, &P, &Q, &D, &E)) != 0 ||
+    (ret = mbedtls_rsa_export_crt(mbedtls_pk_rsa(key), &DP, &DQ, &QP)) != 0;
+  lua_setmpi(L, -2, &N, "n"); lua_setmpi(L, -2, &P, "p"); lua_setmpi(L, -2, &Q, "q"); 
+  lua_setmpi(L, -2, &D, "d"); lua_setmpi(L, -2, &E, "e"); lua_setmpi(L, -2, &DP, "dp"); 
+  lua_setmpi(L, -2, &DQ, "dq"); lua_setmpi(L, -2, &QP, "qp") != 0;
+  if (failure) {
+    char error_buf[1024];
+    mbedtls_strerror(ret, error_buf, sizeof(error_buf));
+    lua_pushnil(L);
+    lua_pushstring(L, error_buf);
+  }
+  mbedtls_mpi_free(&N); mbedtls_mpi_free(&P); mbedtls_mpi_free(&Q);
+  mbedtls_mpi_free(&D); mbedtls_mpi_free(&E); mbedtls_mpi_free(&DP);
+  mbedtls_mpi_free(&DQ); mbedtls_mpi_free(&QP);
+  mbedtls_pk_free(&key);
+  mbedtls_entropy_free(&entropy);
+  mbedtls_ctr_drbg_free(&ctr_drbg);
+  return failure ? 2 : 1;
 }
 
-static int f_generate_csr(lua_State* L) {
-  return 0;
+static int f_create_keypair(lua_State* L) {
+  int ret = 0;
+  mbedtls_pk_context key;
+  mbedtls_entropy_context entropy;
+  mbedtls_ctr_drbg_context ctr_drbg;
+  const unsigned char *pers = "gen_key";
+  mbedtls_entropy_init(&entropy);
+  mbedtls_pk_init(&key);
+  mbedtls_ctr_drbg_init(&ctr_drbg);
+  static int KEY_SIZE = 4096;
+  static int EXPONENT = 65537;
+  unsigned char privkey_buf[16000] = {0};
+  unsigned char pubkey_buf[16000] = {0};
+  int success = 
+    ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, pers, strlen(pers))) != 0) ||
+    ((ret = mbedtls_pk_setup(&key, mbedtls_pk_info_from_type(MBEDTLS_PK_RSA))) != 0) ||
+    ((ret = mbedtls_rsa_gen_key(mbedtls_pk_rsa(key), mbedtls_ctr_drbg_random, &ctr_drbg, KEY_SIZE, EXPONENT)) != 0) || 
+    ((ret = mbedtls_pk_write_key_pem(&key, privkey_buf, sizeof(privkey_buf))) != 0) ||
+    ((ret = mbedtls_pk_write_pubkey_pem(&key, pubkey_buf, sizeof(pubkey_buf))) != 0);
+  mbedtls_pk_free(&key);
+  mbedtls_ctr_drbg_free(&ctr_drbg);
+  mbedtls_entropy_free(&entropy);
+  if (ret) {
+    mbedtls_strerror(ret, privkey_buf, sizeof(privkey_buf));
+    lua_pushnil(L);
+    lua_pushstring(L, privkey_buf);
+  } else {
+    lua_pushstring(L, privkey_buf);
+    lua_pushstring(L, pubkey_buf);
+  }
+  return 2;
+}
+
+static int f_sign_message(lua_State* L) {
+  size_t private_key_length;
+  const char* private_key = luaL_checklstring(L, 1, &private_key_length);
+  size_t message_length;
+  const char* message = luaL_checklstring(L, 2, &message_length);
+  mbedtls_entropy_context entropy;
+  mbedtls_entropy_init(&entropy);
+  mbedtls_ctr_drbg_context ctr_drbg;
+  mbedtls_ctr_drbg_init(&ctr_drbg);
+  mbedtls_pk_context key;
+  mbedtls_pk_init(&key);
+  unsigned char signature[1024] = {0};
+  unsigned char hash[32] = {0};
+  int ret = 0;
+  size_t signature_length;
+  const char* pers = "rsa_sign_pss";
+  if (
+    (ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, pers, strlen(pers))) != 0 ||
+    (ret = mbedtls_sha256(message, message_length, hash, 0)) != 0 ||
+    (ret = mbedtls_pk_parse_key(&key, private_key, private_key_length + 1, NULL, 0, mbedtls_ctr_drbg_random, &ctr_drbg)) != 0 ||
+    (ret = mbedtls_rsa_set_padding(mbedtls_pk_rsa(key), MBEDTLS_RSA_PKCS_V15, MBEDTLS_MD_SHA256)) != 0 ||
+    (ret = mbedtls_pk_sign(&key, MBEDTLS_MD_SHA256, hash, sizeof(hash), signature, sizeof(signature), &signature_length, mbedtls_ctr_drbg_random, &ctr_drbg)) != 0
+   ) {
+    char error_buf[1024];
+    mbedtls_strerror(ret, error_buf, sizeof(error_buf));
+    lua_pushnil(L);
+    lua_pushstring(L, error_buf);
+   } else {
+    lua_pushlstring(L, signature, signature_length);
+  }
+  mbedtls_pk_free(&key);
+  mbedtls_ctr_drbg_free(&ctr_drbg);
+  mbedtls_entropy_free(&entropy);
+  return ret != 0 ? 2 : 1;
+}
+
+static int f_parse_cert(lua_State* L) {
+  size_t certificate_length;
+  const char* certificate = luaL_checklstring(L, 1, &certificate_length);
+  mbedtls_x509_crt crt;
+  mbedtls_x509_crt_init(&crt);
+  int ret = mbedtls_x509_crt_parse(&crt, certificate, certificate_length);
+  if (ret) {
+    mbedtls_x509_crt_free(&crt);
+    lua_pushnil(L);
+    lua_pushliteral(L, "error parsing certificate");
+    return 2;
+  }
+  lua_newtable(L);
+  struct tm time={0};
+  time.tm_sec = crt.valid_to.sec;
+  time.tm_min = crt.valid_to.min;
+  time.tm_hour = crt.valid_to.hour;
+  time.tm_mday = crt.valid_to.day;
+  time.tm_mon = crt.valid_to.mon;
+  time.tm_year = crt.valid_to.year - 1900;
+  lua_pushinteger(L, mktime(&time));
+  lua_setfield(L, -2, "valid_to");
+  mbedtls_x509_crt_free(&crt);
+  return 1;
+}
+
+// https://github.com/Mbed-TLS/mbedtls/blob/mbedtls-3.6/programs/x509/cert_req.c
+static int f_create_csr(lua_State* L) {
+  int ret = 0;
+  const char* pers = "csr_create";
+  size_t private_key_length;
+  const char* private_key = luaL_checklstring(L, 1, &private_key_length);
+  luaL_checktype(L, 2, LUA_TTABLE);
+  const char* domain = NULL;
+  if (lua_rawlen(L, 2) >= 1) {
+    lua_rawgeti(L, 2, 1);
+    domain = luaL_checkstring(L, -1);
+    lua_pop(L, 1);
+  }
+  mbedtls_ctr_drbg_context ctr_drbg;
+  mbedtls_ctr_drbg_init(&ctr_drbg);
+  mbedtls_entropy_context entropy;
+  mbedtls_entropy_init(&entropy);
+  mbedtls_pk_context key;
+  mbedtls_pk_init(&key);
+  mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, pers, strlen(pers));
+  mbedtls_pk_parse_key(&key, private_key, private_key_length + 1, NULL, 0, mbedtls_ctr_drbg_random, &ctr_drbg);
+  mbedtls_x509write_csr req;
+  mbedtls_x509write_csr_init(&req);
+  mbedtls_x509write_csr_set_md_alg(&req, MBEDTLS_MD_SHA256);
+  mbedtls_x509write_csr_set_key(&req, &key);
+  mbedtls_x509write_csr_set_subject_name(&req, domain);
+  mbedtls_x509_san_list* subject_list = NULL;
+  for (int i = 1; i <= lua_rawlen(L, 2); ++i) {
+    mbedtls_x509_san_list* list = calloc(1, sizeof(mbedtls_x509_san_list));
+    list->node.type = MBEDTLS_X509_SAN_DNS_NAME;
+    lua_rawgeti(L, 2, i);
+    list->node.san.unstructured_name.p = (char*)luaL_checklstring(L, -1, &list->node.san.unstructured_name.len);
+    lua_pop(L, 1);
+    list->next = subject_list;
+    subject_list = list;
+  }
+  mbedtls_x509write_csr_set_subject_alternative_name(&req, subject_list);
+  unsigned char buf[4096];  
+  ret = mbedtls_x509write_csr_pem(&req, buf, sizeof(buf), mbedtls_ctr_drbg_random, &ctr_drbg);
+  mbedtls_pk_free(&key);
+  mbedtls_ctr_drbg_free(&ctr_drbg);
+  mbedtls_entropy_free(&entropy);
+  mbedtls_x509write_csr_free(&req);
+  while (subject_list) {
+    mbedtls_x509_san_list* list = subject_list->next;
+    free(subject_list);
+    subject_list = list->next;
+  }
+  if (ret) {
+    mbedtls_strerror(ret, buf, sizeof(buf));
+    return luaL_error(L, "error generating private key: %s", buf);
+  }
+  lua_pushstring(L, buf);
+  return 1;
+}
+
+static int f_sha256(lua_State* L) {
+  size_t len;
+  const char* str = luaL_checklstring(L, 1, &len);
+  char hash[32];
+  mbedtls_sha256(str, len, hash, 0);
+  lua_pushlstring(L, hash, sizeof(hash));
+  return 1;
 }
 
 
 static const luaL_Reg acme_lib[] = {
-  { "generate_keypair",  f_generate_keypair   },
-  { "generate_cert",     f_generate_cert      },
-  { "generate_csr",      f_generate_csr       },
+  { "keypair",    f_create_keypair   },
+  { "sign",       f_sign_message     },
+  { "csr",        f_create_csr       },
+  { "cert",       f_parse_cert       },
+  { "components", f_components       },
+  { "sha256",     f_sha256           },
   { NULL,        NULL }
 };
 
@@ -34,14 +237,15 @@ int main(int argc, char* argv[]) {
   lua_State* L = luaL_newstate();
   luaL_openlibs(L);
   void** extra = lua_getextraspace(L);
+  luaL_requiref(L, "wtk.c", luaopen_wtk_c, 0);
   luaW_requiref(L, "wtk.server.c", luaopen_wtk_server_c);
   luaW_requiref(L, "wtk.client.c", luaopen_wtk_client_c);
   luaW_requiref(L, "wtk.json.c", luaopen_wtk_json_c);
+  luaW_requiref(L, "wtk.proc.c", luaopen_wtk_proc_c);
   lua_pushliteral(L, WTKPROXY_VERSION), lua_setglobal(L, "VERSION");
-  luaL_requiref(L, "wtk.c", luaopen_wtk_c, 0);
   lua_newtable(L);
   luaL_setfuncs(L, acme_lib, 0);
-  lua_setglobal(L, "acme");
+  lua_setglobal(L, "ACME");
   if (luaW_signal(L) || luaW_packlua(L, ".") || luaW_loadentry(L, "init") || luaW_run(L, argc, argv)) {
     fprintf(stderr, "%s\n", lua_tostring(L, -1));
     return -1;
